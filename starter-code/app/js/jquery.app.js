a8d50fd5e5b90e4669e8e7939a7df128fbb83d1a
shockeyt <shockeyt@gmail.com>
2017-04-03 16:47:51 -0600

    added new cat to list

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,6 +1,8 @@
 console.log("js is working");
-
+// var cats = $('#cats').attr('id');
 var cats = document.getElementById('cats');
+var catname = $('#cat-name').val();
+var catnote = $('#cat-note').val();
 
 
 function makeResults() {
@@ -15,17 +17,24 @@ $("form").on("submit", function(event){
 	event.preventDefault();
 	makeResults();
 
-var ajax = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
-	.done(function(data){
-		var separateCats = JSON.parse(data);
-		for (var i = 0; i < separateCats.length; i++) {
-			var catList = document.createElement('li');
+	var ajax = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
+		.done(function(data){
+			var separateCats = JSON.parse(data);
+			for (var i = 0; i < separateCats.length; i++) {
+				var catList = document.createElement('li');
 
-			catList.append(separateCats[i].name + " - " + separateCats[i].note);
-			cats.append(catList);
-		}
-	console.log(data);
-	});
+				catList.append(separateCats[i].name + " - " + separateCats[i].note);
+				cats.append(catList);
+				// console.log(separateCats[0]);
+			}
+		// console.log(data);
+		});
+
+	var newlist = document.createElement('li');
+	var catname = $('#cat-name').val();
+	var catnote = $('#cat-note').val();
+	newlist.append(catname + " - " + catnote);
+	$('.results').append(newlist);
 
 // var catinfo = ajax.innerHTML;
 // JSON.stringify(ajax);
